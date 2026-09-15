8ea47494482dfc331995edb6cebcca6f554f4d9d
tup85417 <tup85417@temple.edu>
2026-09-15 15:03:55 -0400

    Completed assignment

--- app/src/main/java/edu/temple/inclassuiactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiactivity/MainActivity.kt
@@ -25,8 +25,8 @@ class MainActivity : AppCompatActivity() {
 
         with (spinner) {
             /* Step 2: Create adapter to display items from array in Spinner */
-            adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_spinner_item, numberArray)
-
+            //adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_spinner_item, numberArray)
+            adapter = TextSizeAdapter(this@MainActivity, numberArray)
 
             // Step 3: Change TextView's text size to the number selected in the Spinner */
             onItemSelectedListener = object : OnItemSelectedListener {
--- app/src/main/java/edu/temple/inclassuiactivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiactivity/TextSizeAdapter.kt
@@ -1,25 +1,33 @@
 package edu.temple.inclassuiactivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
+import org.w3c.dom.Text
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(private val  context: Context, private val numbers: Array<Int>) : BaseAdapter() {
 
-    override fun getCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getCount() = numbers.size
 
-    override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
-    }
+    override fun getItem(position: Int) = numbers[position]
 
-    override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
-    }
+    override fun getItemId(position: Int) = position.toLong()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val textView = if(convertView == null) {TextView(context)}
+        else {convertView as TextView}
+        textView.text = numbers[position].toString()
+        textView.textSize = 22f
+        return textView
     }
 
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+        val textView = (getView(position, convertView, parent) as TextView)
+
+        textView.textSize = numbers[position].toFloat()
+
+        return textView
+    }
 }
